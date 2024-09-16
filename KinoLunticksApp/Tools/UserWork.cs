e4ce0db4b2746b40f4a75c7454cca188f0318f16
Alex <sasha.shustov.05@mail.ru>
2024-09-16 12:44:15 +0300

    Добавлена функция регистрации новых пользователей; добавлены методы для регистрации, проверки наличия логина и электронной почты в класс работы с пользователями; задокументировал методы в классе работы с пользователями

--- KinoLunticksApp/Tools/UserWork.cs
+++ KinoLunticksApp/Tools/UserWork.cs
@@ -1,4 +1,8 @@
-﻿using KinoLunticksApp.Models;
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using KinoLunticksApp.Models;
+using KinoLunticksApp.Pages;
 
 namespace KinoLunticksApp.Tools
 {
@@ -6,9 +10,89 @@ namespace KinoLunticksApp.Tools
     {
         KinoLunticsContext _db = new KinoLunticsContext();
 
+        /// <summary>
+        /// Проверяет существование пользователя в базе данных с указанным логином и паролем.
+        /// </summary>
+        /// <param name="login">Логин пользователя</param>
+        /// <param name="password">Пароль пользователя</param>
+        /// <returns>Возвращает true, если пользователь с заданными логином и паролем существует, иначе false.</returns>
         public bool IsValid(string login, string password)
         {
-            return _db.Users.Where(user => user.Login ==  login && user.Password == password).Any();
+            return _db.Users.Where(user => user.Login == login && user.Password == password).Any();
+        }
+
+        /// <summary>
+        /// Проверяет существование пользователя в базе данных с указанным логином.
+        /// </summary>
+        /// <param name="login">Логин пользователя</param>
+        /// <returns>Возвращает true, если пользователь с заданным логином существует, иначе false.</returns>
+        public bool IsLoginExists(string login)
+        {
+            return _db.Users.Any(user => user.Login == login);
+        }
+
+        /// <summary>
+        /// Проверяет существование пользователя в базе данных с указанной электронной почтой.
+        /// </summary>
+        /// <param name="email">Электронная почта пользователя</param>
+        /// <returns>Возвращает true, если пользователь с заданной электронной почтой существует, иначе false.</returns>
+        public bool IsEmailEsists(string email)
+        {
+            return _db.Users.Any(user => user.EmailAddress == email);
+        }
+
+        /// <summary>
+        /// Регистрирует нового пользователя в базе данных.
+        /// </summary>
+        /// <param name="userLogin">Логин пользователя</param>
+        /// <param name="password">Пароль пользователя</param>
+        /// <param name="userName">Имя пользователя</param>
+        /// <param name="userLastName">Фамилия пользователя</param>
+        /// <param name="userEmail">Электронная почта пользователя</param>
+        /// <param name="frame">Фрейм для навигации на страницу авторизации</param>
+        /// <param name="birthDate">Дата рождения пользователя (необязательный параметр)</param>
+        public void RegisterUser(string userLogin, string password, 
+                                 string userName, string userLastName, 
+                                 string userEmail, Frame frame,
+                                 DateOnly? birthDate = null)
+        {
+            var defaultRole = _db.Roles.FirstOrDefault(role => role.RoleName == "Пользователь");
+
+            User _user = new User
+            {
+                Login = userLogin,
+                Password = password,
+                UserName = userName,
+                UserLastName = userLastName,
+                EmailAddress = userEmail,
+                BirthDate = birthDate,
+                UserRole = defaultRole.RoleId
+            };
+
+            _db.Users.Add(_user);
+
+            try
+            {
+                _db.SaveChanges();
+
+                MessageBox.Show(
+                    "Информация сохранена",
+                    "Сохранение",
+                     MessageBoxButton.OK,
+                     MessageBoxImage.Information
+                    );
+
+                frame.Navigate(new AutorizationPage(frame));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message.ToString(),
+                    "Системная ошибка",
+                     MessageBoxButton.OK,
+                     MessageBoxImage.Error
+                    );
+            }
         }
     }
 }
