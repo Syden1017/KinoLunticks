29dcae327b6cc94dc31005bdf1789224beed873b
исп-42 <-42@LAB30-04>
2024-09-18 18:03:06 +0300

    добавлены и выведены фильмы а гавную страницу

--- KinoLunticksApp/Pages/MainPage.xaml.cs
+++ KinoLunticksApp/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using KinoLunticksApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Controls;
 
 namespace KinoLunticksApp.Pages
 {
@@ -7,13 +9,46 @@ namespace KinoLunticksApp.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        KinoLunticsContext _db = new KinoLunticsContext();
+        List<Movie> _movies = new List<Movie>();
+
         Frame _frame;
 
         public MainPage(Frame frame)
         {
             InitializeComponent();
 
+            _db.Movies.Load();
+            _movies = _db.Movies.ToList();
+
+            lViewLuntiki.ItemsSource = _movies;
+
             _frame = frame;
+        }
+
+        private void txtBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbBoxFilterField_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbBoxFilterType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbBoxSortField_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbBoxSortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
