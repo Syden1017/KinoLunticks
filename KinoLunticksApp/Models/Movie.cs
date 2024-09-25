5007a99b55a705d8b1924efdfb8062d92e7ded19
исп-42 <-42@LAB30-04>
2024-09-25 17:56:58 +0300

    изменения в главном меню, добавлен и реализован поиск, +немножко стилей хихихи

--- KinoLunticksApp/Models/Movie.cs
+++ KinoLunticksApp/Models/Movie.cs
@@ -1,24 +1,27 @@
-﻿namespace KinoLunticksApp.Models;
+﻿using System;
+using System.Collections.Generic;
+
+namespace KinoLunticksApp.Models;
 
 public partial class Movie
 {
-    public string MovieCode { get; set; } = null!;
+    public string MovieCode { get; set; } = null!;          //Код фильма
 
-    public string MovieName { get; set; } = null!;
+    public string MovieName { get; set; } = null!;          //Название фильма
 
-    public string MovieDescription { get; set; } = null!;
+    public string MovieDescription { get; set; } = null!;   //Описание фильма
 
-    public string MovieGenre { get; set; } = null!;
+    public string MovieGenre { get; set; } = null!;         //Жанр
 
-    public double MovieRating { get; set; }
+    public double MovieRating { get; set; }                 //Рейтинг
 
-    public string MovieDuration { get; set; } = null!;
+    public string MovieDuration { get; set; } = null!;      //Длительность
 
-    public string AgeRestriction { get; set; } = null!;
+    public string AgeRestriction { get; set; } = null!;     //Возратные ограничения
 
-    public decimal TicketPrice { get; set; }
+    public decimal TicketPrice { get; set; }                //Цена
 
-    public byte[]? Preview { get; set; }
+    public byte[]? Preview { get; set; }                    //Картиночка
 
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
