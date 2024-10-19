abf3d3337a44801aa343daae5afd59b05613277c
ИСП-42 <-42@LAB30-03>
2024-10-19 10:45:14 +0300

    выведена информация о покупке билета

--- KinoLunticksApp/Windows/PaymentWindow.xaml.cs
+++ KinoLunticksApp/Windows/PaymentWindow.xaml.cs
@@ -23,6 +23,40 @@ namespace KinoLunticksApp.Windows
             _user = user;
             _seats = seats;
             _fullAmount = fullAmount;
+
+            DataContext = new
+            {
+                moviePreview = _movie.Preview,
+                movieName = _movie.MovieName,
+                movieAgeRestriction = _movie.AgeRestriction,
+                movieDuration = _movie.MovieDuration,
+                seats = _seats,
+                fullAmount = _fullAmount
+            };
+        }
+
+        private void btnPay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                                                    "Вы действительно хотите отменить операцию?",
+                                                    "Отмена оплаты",
+                                                    MessageBoxButton.YesNo,
+                                                    MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Close();
+            }
         }
     }
 }
